e09de1b61649312a7fc4f68649a38c325722d83e
Samuel Roberto <samuel.roberto@futura-dev.com>
2026-04-09 09:56:41 +0200

    feat: library refactoring and  added change permission endpoints

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,33 @@ import { GraphApiService } from "./graph-api/graph-api.service";
 import { GraphApiModule } from "./graph-api/graph-api.module";
 import { SharepointApiService } from "./sharepoint-api/sharepoint-api.service";
 import { SharepointApiModule } from "./sharepoint-api/sharepoint-api.module";
+import {
+  DriveItem,
+  DriveItemPermission,
+  DriveItemPermissionsResponse,
+  DriveRecipient,
+  InviteDriveItemPermissionsResponse,
+  ListItem,
+  ListLog,
+  ListWebhook,
+  HRListItem,
+} from "./sharepoint-api/types";
 
 export {
   GraphApiService,
   GraphApiModule,
   SharepointApiService,
-  SharepointApiModule
+  SharepointApiModule,
 };
-// export type { ListWebhook, ListLog, ListItem, HRListItem }
+
+export type {
+  DriveItem,
+  DriveItemPermission,
+  DriveItemPermissionsResponse,
+  DriveRecipient,
+  InviteDriveItemPermissionsResponse,
+  ListWebhook,
+  ListLog,
+  ListItem,
+  HRListItem,
+};
